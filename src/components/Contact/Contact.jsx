2f08b802e375f96d4da90e68ef21d0060f089ad1
raj123215 <rajkumar.123215@marwadiuniversity.ac.in>
2025-05-22 16:50:46 +0530

    final

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_olm3773", // Replace with your EmailJS Service ID
+        "template_j81ucwx", // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "tJMhGD_lZVUUda9f_" // Replace with your EmailJS Public Key
       )
       .then(
         () => {
